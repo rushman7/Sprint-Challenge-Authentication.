f22140c8681ccc46386d669c7cda0c6b40d8ee38
BuildTools <arthurpisakhov1@gmail.com>
2019-11-10 20:20:41 -0500

    added middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,33 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+module.exports = { 
+  validateCredentialBody, 
+  restricted,
+  generateToken
 };
+
+function validateCredentialBody(req, res, next) {
+  const { username, password } = req.body;
+
+  if (!req.body) res.status(400).json({ message: "Missing multiple BODY inputs." })
+  else if (!username || !password) res.status(400).json({ message: "Missing username or password." })
+  else next();
+}
+
+function restricted(req, res, next) {
+  if (req.session && req.session.user) next();
+  else res.status(401).json({ message: 'You shall not pass!' })
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const secret = 'qwdqwldq9u129dj1l2du1o2d12';
+  const options = {
+    expiresIn: '8h',
+  };
+
+  return jwt.sign(payload, secret, options)
+}
